859d30d83985163ef73c69762b947c8b02931e26
TheStoryOfCampion <94606154+TheStoryOfCampion@users.noreply.github.com>
2023-10-19 16:25:29 +0200

    bug fixed

--- eu_staff/lib/main.dart
+++ eu_staff/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:eu_staff/Pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
